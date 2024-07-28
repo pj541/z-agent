a99663696599b553c195d446757a8558175e5d2e
pj541 <98407073+pj541@users.noreply.github.com>
2024-07-29 02:00:44 +0530

    revert back getpass

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 # VERSION = '{{VERSION_PLACEHOLDER}}'
 setuptools.setup(
     name="z-agent",
-    version="0.1.1",
+    version="0.1.1.0",
     author="Prajwal",
     author_email="pkumarjha@zscaler.com",
     long_description_content_type="text/markdown",
@@ -12,7 +12,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["websockets", "asyncio", "getpass"],
+    install_requires=["websockets", "asyncio"],
     packages=setuptools.find_packages(),
     python_requires=">=3.8",
     entry_points={
